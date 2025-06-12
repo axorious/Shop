3957f48505ea4f7faf0ceda52127069c8127a25e
axorious <makz-2001@mail.ru>
2025-06-12 15:12:29 +0300

    upd app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -9,7 +10,7 @@ const routes = require("./routes");
 const port = 3001;
 const app = express();
 
-app.use(express.static("../frontend/dist"));
+app.use(express.static(path.resolve("..", "frontend", "dist")));
 app.use(
   cors({
     origin: [
@@ -25,6 +26,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("..", "frontend", "build", "index.html"));
+});
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   app.listen(port, () => {
